a7a7611dd90edb78bddb7c187eb90b7e5938c3cf
Jose Angel <jyaxa@yaxa.co>
2016-10-01 16:21:33 -0400

    actualizando debug

--- src/Core/Database/DB.php
+++ src/Core/Database/DB.php
@@ -1,5 +1,6 @@
 <?php
 namespace V2\Core\Database;
+use ErrorHandler;
 use Exception;
 use PDOException;
 use PDO;
@@ -111,7 +112,7 @@ class DB {
         catch (PDOException $e) {
             $trace = explode(":",$e->getMessage());
             // si => SQLSTATE[HY000]: General error: 2006 MySQL server has gone away
-            $error = new ErrorHandler($e->getMessage(),"PDOEXCEPTION",$this->code_errors["parse"],$e);
+            $error = new \ErrorHandler($e->getMessage(),"PDOEXCEPTION",$this->code_errors["parse"],$e);
             $error->setData("query",$query);
             if (sizeof($trace)>3) {
                 $SQLSTATE = trim(substr($trace[2],0,6));
