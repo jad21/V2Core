bc181a63c3b9937f09726992d79bbe07190a11d8
Jose Angel <jyaxa@yaxa.co>
2016-10-07 17:24:25 -0400

    update cache remove

--- src/Core/Cache/Cache.php
+++ src/Core/Cache/Cache.php
@@ -4,7 +4,7 @@ namespace V2\Core\Cache;
 abstract class Cache
 {
     protected static $directory = VAR_DIRECTORY . "cache";
-    public static function put($key, $value)
+    public static function put($key, $value,$update_force = false)
     {
         if (!is_dir(self::$directory)) {
             mkdir(self::$directory);
@@ -12,7 +12,7 @@ abstract class Cache
         $hash = md5($key);
         $file = self::$directory . "/{$hash}.cache";
         // check the bloody file.
-        if (!file_exists($file)) {
+        if (!file_exists($file) OR $update_force) {
             $bool = file_put_contents($file, $value);
             return true;
         }
@@ -30,6 +30,14 @@ abstract class Cache
     public static function has($key)
     {
         return self::get($key, false) ? true : false;
+    }
+    public static function remove($key)
+    {
+        $hash = md5($key);
+        $file = self::$directory . "/$hash.cache";
+        if (file_exists($file)) {
+            unlink($file);
+        }
     }
 
 }
